86593df80623e57173eeb89692d700a7bd4d7bea
Carter Carlson <carterbud1@gmail.com>
2018-07-04 15:19:16 -0500

    Started work on duplicate image valdiation

--- validate white border.py
+++ validate white border.py
@@ -3,24 +3,51 @@ import shutil
 from PIL import Image
 
 dir = '//hqfas322003c.corp.drugstore.com/cnc1shared/Files to Clean/Walgreens.com/Images to process/'
-dir_border = '//hqfas322003c.corp.drugstore.com/cnc1shared/Files to Clean/Walgreens.com/Bad image border/'
-bad_borders = []
+dir_border = '//hqfas322003c.corp.drugstore.com/cnc1shared/Files to Clean/Walgreens.com/REJECTED/Bad image border/'
+dir_duplicate = '//hqfas322003c.corp.drugstore.com/cnc1shared/Files to Clean/Walgreens.com/REJECTED/Duplicate image/'
 
-for file in os.listdir(dir):
-    if file.endswith('db'):
+bad_borders = []
+duplicate_images = []
+
+files = []
+[files.append(file) for file in os.listdir(dir)]
+
+for file in range(0, len(files)):
+    if files[file].endswith('db'):
         continue
-    im = Image.open(dir + file)
-    pix = im.load()
-    pic_size = im.size
-    width = pic_size[0]
-    height = pic_size[1]
-    avg_border = (width + height)/2
-    pix_req = int(round(avg_border * 0.016))
+    im1 = Image.open(dir + files[file])
+    pix = im1.load()
+    pic_size = im1.size
+    width, height = pic_size
+    avg_dimension = (width + height)/2
+    pix_req = int(round(avg_dimension * 0.016))
     mid_height = height - pix_req * 2
     white_pixels = []
 
-    '''
+    file1 = files[file]
+    if '_' in files[file]:
+        file1_upc = files[file][:files[file].find('_')]
+    else:
+        file1_upc = files[file][:files[file].find('.')]
 
+    next_image = 1
+    while True:
+        file2 = files[file + next_image]
+        im2 = Image.open(dir + file2)
+        if '_' in files[file + next_image]:
+            file2_upc = files[file + next_image][:files[file + next_image].find('_')]
+        else:
+            file2_upc = files[file + next_image][:files[file + next_image].find('.')]
+        if file1_upc != file2_upc:
+            break
+        else:
+            next_image += 1
+            if compare_images(file1, file2):
+                duplicate_images.append(file1)
+                duplicate_images.append(file2)
+
+
+    '''
     How dimensions for white border are calculated
     ----------------------------------------------
     top
@@ -44,25 +71,48 @@ for file in os.listdir(dir):
         b. This tests the border without validating EVERY pixel
         c. For top and bottom, it gets every 10th wide pixel, and every other height pixel
         d. for left and right, it gets every 10th height pixel, and every other width pixel
-
     '''
 
-    for a in range(1, int(pix_req/2)):
+    for a in range(1, int(pix_req), 2):
         # pix[width, height]
-        for b in range(1, int(width/10)):
+        for b in range(1, int(width), 10):
             # Top portion
-            white_pixels.extend(pix[b*10, a*2])
+            white_pixels.extend(pix[b, a])
             # Bottom portion
-            white_pixels.extend(pix[b*10, height - pix_req + a*2])
+            white_pixels.extend(pix[b, height - pix_req + a])
         # Two middle portions - between top and bottom portion
-        for b in range(1, int(mid_height/10)):
+        for b in range(1, int(mid_height), 10):
             # Left side
-            white_pixels.extend(pix[a*2, pix_req - 1 + b*10])
+            white_pixels.extend(pix[a, pix_req - 1 + b])
             # Right side
-            white_pixels.extend(pix[width - pix_req - 1 + 2*a , pix_req - 1 + b*10])
+            white_pixels.extend(pix[width - pix_req - 1 + a, pix_req - 1 + b])
 
-    if not (all(for i in white_pixels if i == 255)):
-        bad_borders.append(file)
+    if not(all(for i in white_pixels if i == 255)):
+        bad_borders.append(files[file])
+
+
+
+
+def compare_images(image1, image2):
+    # Add: convert to greyscale
+    pix1 = image1.load()
+    pix2 = image2.laod()
+    pic_size1 = im1.pic_size
+    pic_size2 = im2.pic_size1
+    if sum(pic_size1) == sum(pic_size2):
+        continue
+    else:
+        # pick which image is larger
+        # resize other image to larger image
+
+    if image1 != 95% of image2:
+        return False
+    else:
+        return True
 
 for file in bad_borders:
     shutil.move(dir + file, dir_border + file)
+
+duplicate_images = n_unique(duplicate_images)
+for file in duplicate_images:
+    shutil.move(dir + file, dir_duplicate + file)
