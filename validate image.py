e0e10d2307c557a3270d90ebf961064d690401ef
Carter Carlson <carterbud1@gmail.com>
2018-07-25 09:18:14 -0500

    Clean 'for' loops / only move alt images to duplicate folder / add .gitignore

--- validate image.py
+++ validate image.py
@@ -24,10 +24,10 @@ def images_match(file1, file2):
     difference = cv2.subtract(image1, image2)
 
     # Give 10px margin of error in case image is slightly tinted
-    pix_match = (difference >= 0).sum()
+    pix_match = (difference >= 10).sum()
     percent_similar = pix_match / len(difference)
 
-    if percent_similar > 0.90:
+    if percent_similar > 0.95:
         return True
 
 def missing_border(image):
@@ -41,27 +41,27 @@ def missing_border(image):
     width = image.shape[1]
 
     # Loop over image
-    # White rgb = [255, 255, 255] or 765 - give 10 px of leeway = 755
+    # White rgb = [255, 255, 255] or 765 - give 20 px of margin = 745
     for a in range(1, pix_req, 2):
 
         # Check corner borders
         if (image[a, a].sum() or
             image[width - a, a].sum() or
             image[a, height - a].sum() or
-            image[width - a, height - a].sum()) < 755:
+            image[width - a, height - a].sum()) < 745:
             return True
 
         # Check vertical borders
         if v_border:
             for b in range(pix_req, width - pix_req, 10):
-                if (image[b, a].sum() or image[b, height - a].sum()) < 755:
+                if (image[b, a].sum() or image[b, height - a].sum()) < 745:
                     v_border = False
                     break
 
         # Check horizontal borders
         if h_border:
             for b in range(pix_req, height - pix_req, 10):
-                if(image[a, b].sum() or image[width - a, b].sum()) < 755:
+                if(image[a, b].sum() or image[width - a, b].sum()) < 745:
                     h_border = False
                     break
 
@@ -80,51 +80,41 @@ files = []
 [files.append(file) for file in os.listdir(dir)]
 
 # Compare duplicate images
-for file in range(0, len(files) - 1):
-    if files[file].endswith('db'):
+for i in range(0, len(files) - 1):
+    file1 = files[i]
+    file1_upc = file1[:file1.find('.')]
+    next_image = 1
+
+    if file1.endswith('db') or '_' in file1:
         continue
 
-    file1 = files[file]
-    # Grab upc for file 1
-    if '_' in files[file]:
-        file1_upc = files[file][:files[file].find('_')]
-    else:
-        file1_upc = files[file][:files[file].find('.')]
-    next_image = 1
     while True:
         try:
-            file2 = files[file + next_image]
-            # Grab upc for file 2
-            if '_' in files[file + next_image]:
-                file2_upc = files[file + next_image][:files[file + next_image].find('_')]
-            else:
-                file2_upc = files[file + next_image][:files[file + next_image].find('.')]
+            file2 = files[i + next_image]
+            file2_upc = file2[:file2.find('_')]
+            
             # See if the upc's match
             if file1_upc != file2_upc:
                 break
             elif images_match(file1, file2):
-                duplicates.append(file1)
                 duplicates.append(file2)
         except:
             break
         next_image += 1
 
-# Keep one copy of each image file
-duplicates = set(duplicates)
-
-# Remove duplicate images so we don't test them for border
-files = set(files) - duplicates
-files = list(files)
+# Remove duplicate images so we don't test them for white border
+files_remaining = list(set(files) - set(duplicates))
 
 # Check image for white border
-for file in range(0, len(files)):
-    if files[file].endswith('db'):
+for i in range(0, len(files_remaining)):
+    file = files_remaining[i]
+    
+    if file.endswith('db'):
         continue
 
-    image = cv2.imread(dir + files[file])
-
+    image = cv2.imread(dir + file)
     if missing_border(image):
-        borders.append(files[file])
+        borders.append(file)
 
 # Move files
 for file in borders:
